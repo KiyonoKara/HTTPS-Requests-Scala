38f9ce81552f45e66e51da303455adf82446f499
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-20 20:07:46 -0700

    Fully moved tests

--- src/main/scala/GetRequest.scala
+++ src/main/scala/GetRequest.scala
@@ -98,18 +98,3 @@ class GetRequest(var url: String) {
     str1.equals(str2)
   }
 }
-
-// Testing here
-object GetRequest {
-  def main(args: Array[String]): Unit = {
-    // With default compression
-    val getRequest: GetRequest = new GetRequest("https://docs.scala-lang.org")
-    val data = getRequest.GET(getRequest.url, Array(Array("Accept", "*/*"), Array("User-Agent", "*")))
-    println(data)
-
-    // With compression disabled by the parameters, but enabled through headers
-    val getRequest2: GetRequest = new GetRequest("https://docs.scala-lang.org")
-    val data2 = getRequest2.GET(getRequest2.url, Array(Array("Accept", "*/*"), Array("User-Agent", "*"), Array("Accept-Encoding", "gzip")))
-    println(data2)
-  }
-}
