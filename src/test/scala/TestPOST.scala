40f372f1220731974f33154f5b357a070e27cfe4
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-23 10:38:26 -0700

    Test now contains headers

--- src/test/scala/TestPOST.scala
+++ src/test/scala/TestPOST.scala
@@ -1,7 +1,11 @@
 object TestPOST {
   def main(args: Array[String]): Unit = {
-    val postRequest: PostRequest = new PostRequest("https://reqres.in/api/users", "{\"name\": \"Li Xi\", \"job\": \"Scala POST\"}")
-    val post = postRequest.POST()
+    val postRequest: PostRequest = new PostRequest("https://reqres.in/api/users")
+    val post = postRequest.POST(
+      "https://reqres.in/api/users",
+      "{\"name\": \"Li Xi\", \"job\": \"Scala POST\"}",
+            Array(Array("Content-Type", "application/json; charset=UTF-8"), Array("User-Agent", "Scala"), Array("Accept", "application/json"))
+    )
     println(post)
   }
 }
