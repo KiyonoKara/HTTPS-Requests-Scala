9de7ef5ad40e3c96aa1185e4727f005e5b9aab60
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-22 21:41:07 -0700

    Removed tests; Those tests have been converted to documentation

--- src/main/scala/util/HandleHeaders.scala
+++ src/main/scala/util/HandleHeaders.scala
@@ -16,10 +16,14 @@ import scala.collection.immutable.HashMap
 class HandleHeaders(var headers: mutable.HashMap[String, String] = mutable.HashMap.empty[String, String]) {
   /** Lower cases the headers by setting them all to lowercase, this is not recommended for authorization tokens and may invalidate it
    *
+   *    Example
+   *    val handleHeaders: HandleHeaders = new HandleHeaders()
+   *    val headers: mutable.HashMap[String, String] = handleHeaders.lowerCaseHeaders(mutable.HashMap("Accept" -> " *", " Accept - Encoding" -> "gzip ", " Authorization" -> " < Credentials > "))
+   *
    * @param headers - HashMap of headers with the key and value
    * @return
    */
-  def lowerCaseHeaders(headers: mutable.HashMap[String, String] = this.headers): mutable.HashMap[String, String] = {
+  def lowerCaseHeaders(headers: mutable.HashMap[String, String] = this.headers): mutable.Map[String, String] = {
     if (headers.nonEmpty) {
       headers.foreach(hash => {
         val k: String = hash._1; val v: String = hash._2
@@ -46,11 +50,4 @@ class HandleHeaders(var headers: mutable.HashMap[String, String] = mutable.HashM
       })
     }
   }
-}
-
-// Testing here
-object HandleHeaders extends App {
-  val handleHeaders: HandleHeaders = new HandleHeaders()
-  val headers: mutable.HashMap[String, String] = handleHeaders.lowerCaseHeaders(mutable.HashMap("Accept" -> "*/*", "Accept-Encoding" -> "gzip", "Authorization" -> "<Credentials>"))
-  println(headers)
 }
