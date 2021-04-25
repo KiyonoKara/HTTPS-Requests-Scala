9fc9f8fc49f83e794f930eb0bd75e03bfb109391
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-24 20:12:08 -0700

    Added Constants

---
+++ src/main/scala/util/Constants.scala
@@ -0,0 +1,33 @@
+package util
+
+/**
+ * Created by KaNguy - 04/24/2021
+ * File util.Constants.scala
+ */
+
+object Constants {
+  /**
+   * Supported methods
+   */
+  /** Read-only; Represents full resource and is meant for only getting data */
+  val GET: String = "GET"
+  /** Read-only; Asks for a response without the body or data */
+  val HEAD: String = "HEAD"
+  /** Writable; Used for submitting entities, bodies, or data, usually results in additions */
+  val POST: String = "POST"
+  /** Writable; Replaces target with the requests's data or payload */
+  val PUT: String = "PUT"
+  /** Writable; Removes a resource or entity; Many APIs will require an authorization header along with this kind of request */
+  val DELETE: String = "DELETE"
+  /** Writable; Applies partial modifications to a resource or entity */
+  val PATCH: String = "PATCH"
+
+
+  /**
+   * Unsupported methods
+   */
+  /** Read-only; Returns 'Allow' headers, CORS and maybe not practical */
+  val OPTIONS: String = "OPTIONS"
+  /** Connection; Tries to establish an HTTP tunnel, mainly for SSL connections */
+  val CONNECT: String = "CONNECT"
+}
