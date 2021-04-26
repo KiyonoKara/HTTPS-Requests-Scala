eecbe20314f33c29079526bd641d366a182a11c2
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-26 09:59:23 -0700

    ...

--- src/test/scala/TestDELETE.scala
+++ src/test/scala/TestDELETE.scala
@@ -3,7 +3,7 @@ import HTTPSRequestsScala.DeleteRequest
 object TestDELETE {
   def main(args: Array[String]): Unit = {
     val deleteRequest: DeleteRequest = new DeleteRequest("https://reqbin.com/sample/delete/json")
-    val data = deleteRequest.DELETE(deleteRequest.url, Array(Array("Accept", "*/*"), Array("User-Agent", "*")))
-    println(data)
+    val output = deleteRequest.DELETE(deleteRequest.url, Array(Array("Accept", "*/*"), Array("User-Agent", "*")))
+    println(output)
   }
 }
