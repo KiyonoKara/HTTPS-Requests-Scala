467819bbf2dc19fdf7a5223f190a903b4f870d46
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-27 09:00:44 -0700

    Moved PutRequest.scala to a new package which has each class dedicated to each method of the requests

--- src/main/scala/HTTPSRequestsScala/Single/PutRequest.scala
+++ src/main/scala/HTTPSRequestsScala/Single/PutRequest.scala
@@ -1,4 +1,4 @@
-package HTTPSRequestsScala
+package HTTPSRequestsScala.Single
 
 /**
  * Created by KaNguy - 04/17/2021
@@ -18,7 +18,7 @@ import java.nio.charset.StandardCharsets
 import scala.io.Source.fromInputStream
 
 // Utilities
-import util.{Convert, HandleHeaders, Constants}
+import util.{Constants, Convert, HandleHeaders}
 
 class PutRequest(var url: String, var data: String = "{}") {
   private val requestMethod: String = Constants.PUT
