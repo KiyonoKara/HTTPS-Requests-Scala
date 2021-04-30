7f0eacdd405b9561273cf69453cf30a5bfc1c872
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-29 22:21:01 -0700

    Update Request.scala

--- src/main/scala/HTTPSRequestsScala/Request.scala
+++ src/main/scala/HTTPSRequestsScala/Request.scala
@@ -5,6 +5,27 @@ package HTTPSRequestsScala
  * File HTTPSRequestsScala.Request.scala
  */
 
-class Request {
+// Data streaming and serialization
+import java.io.{InputStreamReader, Reader}
 
+// Networking and web
+import java.net.{HttpURLConnection, URL}
+
+// Scala IO Source
+import scala.io.Source.{fromInputStream, fromURL}
+
+// Utils
+import java.util.zip.GZIPInputStream
+
+// Local utilities
+import util.{Constants, Convert, HandleHeaders}
+
+class Request(var url: String, var method: String = "GET", headers: Array[Array[String]] = Array[Array[String]]()) {
+  def request(url: String = this.url, method: String = this.method, headers: Array[Array[String]] = this.headers, data: String = null): String = {
+    if (method.toUpperCase.equals(Constants.GET)) {
+
+    }
+    ""
+  }
+  // TODO: Finish this by this weekend? (No ETA)
 }
