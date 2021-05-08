b78736934ff8c0504bf64019e09021ba14793291
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-08 13:52:24 -0700

    Updated tests for the DeleteRequest class

--- src/test/scala/Single/TestDELETE.scala
+++ src/test/scala/Single/TestDELETE.scala
@@ -5,7 +5,7 @@ import HTTPSRequestsScala.Single.DeleteRequest
 object TestDELETE {
   def main(args: Array[String]): Unit = {
     val deleteRequest: DeleteRequest = new DeleteRequest("https://reqbin.com/sample/delete/json")
-    val output = deleteRequest.DELETE(deleteRequest.url, Array(Array("Accept", "*/*"), Array("User-Agent", "*")))
+    val output = deleteRequest.DELETE(deleteRequest.url, Map("Accept" -> "*/*", "User-Agent" -> "*"))
     println(output)
   }
 }
