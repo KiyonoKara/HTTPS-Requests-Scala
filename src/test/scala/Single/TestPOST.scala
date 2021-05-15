f38555de50546fbf52c028946147986f4f0da5a9
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-15 15:30:38 -0700

    Update: Update tests to use the MapToJSON method

--- src/test/scala/Single/TestPOST.scala
+++ src/test/scala/Single/TestPOST.scala
@@ -1,6 +1,7 @@
 package Single
 
 import HTTPSRequestsScala.Single.PostRequest
+import util.Utility
 
 object TestPOST {
   def main(args: Array[String]): Unit = {
@@ -11,5 +12,13 @@ object TestPOST {
             Map("Content-Type" -> "application/json; charset=UTF-8", "User-Agent" -> "Scala", "Accept" -> "application/json")
     )
     println(post)
+
+    val postReq2: PostRequest = new PostRequest("https://reqres.in/api/users")
+    val post2 = postReq2.POST(
+      "https://reqres.in/api/users",
+      Utility.MapToJSON(Map("name"-> "Li Xi", "job" -> "Scala POST")),
+      Map("Content-Type" -> "application/json; charset=UTF-8", "User-Agent" -> "Scala", "Accept" -> "application/json")
+    )
+    println(post2)
   }
 }
