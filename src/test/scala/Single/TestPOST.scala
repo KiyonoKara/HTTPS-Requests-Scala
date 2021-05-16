6597fa2b40ea79fd2d728026694bed9e27059b54
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-16 12:17:31 -0700

    Refactor: Change MapToJSON call to singleMapToJSON

--- src/test/scala/Single/TestPOST.scala
+++ src/test/scala/Single/TestPOST.scala
@@ -16,7 +16,7 @@ object TestPOST {
     val postReq2: PostRequest = new PostRequest("https://reqres.in/api/users")
     val post2 = postReq2.POST(
       "https://reqres.in/api/users",
-      Utility.MapToJSON(Map("name"-> "Li Xi", "job" -> "Scala POST")),
+      Utility.singleMapToJSON(Map("name"-> "Li Xi", "job" -> "Scala POST")),
       Map("Content-Type" -> "application/json; charset=UTF-8", "User-Agent" -> "Scala", "Accept" -> "application/json")
     )
     println(post2)
