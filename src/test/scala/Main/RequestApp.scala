2a5437998f65fdce0480dd9a155e781b707c7ac6
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-18 16:43:31 -0700

    Tests: Refactor method call(s)

--- src/test/scala/Main/RequestApp.scala
+++ src/test/scala/Main/RequestApp.scala
@@ -23,7 +23,7 @@ object RequestApp extends App {
     "http://localhost:8080/payload",
     "POST",
     Map("Content-Type" -> "application/json; charset=UTF-8", "User-Agent" -> "Scala", "Accept" -> "application/json"),
-    requester.CollectionToJSON(
+    requester.collectionToJSON(
       Map("payload" ->
         Map("message" -> "This is a payload",
           "list" -> List(1, 2, 3, 4)
