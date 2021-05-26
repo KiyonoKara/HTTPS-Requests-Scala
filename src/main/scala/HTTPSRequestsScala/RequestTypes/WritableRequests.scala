0cc7004b70e57a0b067fe16c79c61ce023d589c1
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-26 13:00:49 -0700

    Documentation: Update documentation for WritableRequests

--- src/main/scala/HTTPSRequestsScala/RequestTypes/WritableRequests.scala
+++ src/main/scala/HTTPSRequestsScala/RequestTypes/WritableRequests.scala
@@ -32,11 +32,11 @@ class WritableRequests() {
 
   /**
    *
-   * @param url - String; Provide an url for the request
-   * @param method - String; Provide a method, there are no defaults since all are formatted the same but have vastly different outcomes.
-   * @param data - String; Usually data is provided as JSON in the form of a string, but any data in the form of a string is accepted.
-   * @param headers - Iterable[(String, String)]; Headers in the form of a Map collection is primarily valid
-   * @return output - String; Most writable requests have output, thus, it will always guarantee a string
+   * @param url Provide an url for the request
+   * @param method Provide a method, there are no defaults since all are formatted the same but have vastly different outcomes.
+   * @param data Usually data is provided as JSON in the form of a string, but any data in the form of a string is accepted.
+   * @param headers Iterable[(String, String)]; Headers in the form of a Map collection is primarily valid
+   * @return output Most writable requests have output, thus, it will always guarantee an output as a string
    */
   def request(url: String, method: String, data: String = null, headers: Iterable[(String, String)] = Nil): String = {
     // Meant for POST, PUT, PATCH, and DELETE requests
