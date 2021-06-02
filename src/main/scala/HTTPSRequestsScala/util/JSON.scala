96fe99b4f8fa613b6ded7fe4fe76d5db1c88b2c3
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-06-01 17:14:51 -0700

    Fix: More additions and structuring

--- src/main/scala/HTTPSRequestsScala/util/JSON.scala
+++ src/main/scala/HTTPSRequestsScala/util/JSON.scala
@@ -20,7 +20,52 @@ class JSON {
     }
   }
 
+  object Tokenizer {
+    val LeftCurlyBrace: TokenMatcher = TokenMatcher("\\{")
+    val RightCurlyBrace: TokenMatcher = TokenMatcher("\\}")
+    val LeftSquareBracket: TokenMatcher = TokenMatcher("\\[")
+    val RightSquareBracket: TokenMatcher = TokenMatcher("\\]")
+    val Colon: TokenMatcher = TokenMatcher(":")
+    val Comma: TokenMatcher = TokenMatcher(",")
+    val IsTrue: TokenMatcher = TokenMatcher("true")
+    val IsFalse: TokenMatcher = TokenMatcher("false")
+    val IsNull: TokenMatcher = TokenMatcher("null")
+    val String: TokenMatcher = TokenMatcher("\".*?\"")
+    val Number: TokenMatcher = TokenMatcher("[-+]?[0-9]*[\\,\\.]?[0-9]+([eE][-+]?[0-9]+)?")
 
+    class TokenMatcher(partialRegex: String) {
+      private val regex = s"""^($partialRegex)""".r
+
+      def unapply(string: String): Option[String] = regex.findFirstIn(string)
+    }
+
+    object TokenMatcher {
+      def apply(partialRegex: String) = new TokenMatcher(partialRegex)
+    }
+
+    def tokenize(json: String, tokens: List[Token] = List()): List[Token] = {
+      val trimmedJSON = json.trim
+      def continue(token: Token): List[Token] = {
+        tokenize(trimmedJSON.substring(token.value.length), token :: tokens)
+      }
+
+      trimmedJSON match {
+        case "" => tokens.reverse
+        case LeftCurlyBrace(s) => continue(Token.LEFT_CURLY_BRACE)
+        case RightCurlyBrace(s) => continue(Token.RIGHT_CURLY_BRACE)
+        case LeftSquareBracket(s) => continue(Token.LEFT_SQUARE_BRACKET)
+        case RightSquareBracket(s) => continue(Token.RIGHT_SQUARE_BRACKET)
+        case Colon(s) => continue(Token.COLON)
+        case Comma(s) => continue(Token.COMMA)
+        case IsTrue(s) => continue(Token.TRUE)
+        case IsFalse(s) => continue(Token.FALSE)
+        case IsNull(s) => continue(Token.NULL)
+        case String(s) => continue(Token.StringToken(s))
+        case Number(s) => continue(Token.NumberToken(s))
+        case error => println(s"""Could not complete action: $error"""); tokens.reverse
+      }
+    }
+  }
 
   case class JSONException(JSON: String, throwable: Throwable = null) extends RuntimeException(s"Could not parse: $JSON", throwable)
   case class JSONObjectNotFound(JSONObjectName: String, throwable: Throwable) extends RuntimeException(s"""Could not find any JSON object named, "$JSONObjectName"""", throwable)
