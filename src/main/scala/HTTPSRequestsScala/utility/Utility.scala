528de067b03f61ab4d4dd45f0284a910af7dec5d
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-19 23:51:33 -0700

    Feature / Refactor: The Constants object is now embedded in the Utility object, this will make it easier to access since it is a utility after all

--- src/main/scala/HTTPSRequestsScala/utility/Utility.scala
+++ src/main/scala/HTTPSRequestsScala/utility/Utility.scala
@@ -97,5 +97,41 @@ object Utility {
       }
     }
   }
+
+  /**
+   * HTTP-related constants
+   * {{{
+   *   import HTTPSRequestsScala.util.Constants
+   *   val GET: String = Constants.GET
+   * }}}
+   */
+  object Constants {
+    /**
+     * Main HTTP/HTTPS methods
+     */
+    val GET: String = "GET"
+    val POST: String = "POST"
+    val DELETE: String = "DELETE"
+    val PUT: String = "PUT"
+    val HEAD: String = "HEAD"
+    val OPTIONS: String = "OPTIONS"
+    val PATCH: String = "PATCH"
+
+    /**
+     * Environment HTTP/HTTPS methods
+     */
+    val CONNECT: String = "CONNECT"
+    val TRACE: String = "TRACE"
+
+    /**
+     * Officially supported methods
+     */
+    val HTTPMethods: Set[String] = Set(GET, POST, DELETE, PUT, HEAD, OPTIONS, TRACE)
+
+    /**
+     * Timeouts
+     */
+    val DEFAULT_TIMEOUT: Int = 5000
+  }
 }
 
